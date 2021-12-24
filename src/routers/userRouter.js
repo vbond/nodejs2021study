0eabc9f06beed4b290f7c8f5f72f98ea237c6815
Vitaliy Bondarenko <vitaliy.bondarenko@wickes.co.uk>
2021-12-24 19:56:39 +0200

    resolved comments homework3

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -5,6 +5,13 @@ import logger from "../logger/logger.js";
 
 const router = express.Router();
 
+router.use((req, res, next) => {
+    logger.info("req.method=" + req.method);
+    logger.info("req.url=" + req.url);
+    logger.info("req.body=" + JSON.stringify(req.body));
+    next();
+});
+
 router.get('/users/:id', readUser);
 
 router.get('/users', validatorQuery, autoSuggest);
@@ -20,6 +27,10 @@ router.use((err, req, res, next) => {
     logger.error(`req.url: ${req.url}`);
     logger.error(`req.body: ${JSON.stringify(req.body)}`);
     logger.error(`message: ${err}`);
+
+    if (!res.statusCode || res.statusCode === 200) {
+        res.status(500);
+    }
 
     res.send(err);
 });
